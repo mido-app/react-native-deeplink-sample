d425d22a10a635d3b85631cea18b855703a3a5c5
Yutaka Omido <yutaka.omido@gmail.com>
2019-11-20 06:08:59 +0900

    get and confirm permission for using device location

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import * as Permissions from 'expo-permissions'
 
 export default function App() {
+  const checkPermission = async function () {
+    const { status, permissions } = await Permissions.getAsync(Permissions.LOCATION)
+    if (status === 'granted') {
+      alert(`You have permission for using device location.`)
+    } else {
+      alert('You do not have permission for using device location.')
+    }
+    console.log(permissions)
+  }
+
+  const getPermission = async function () {
+    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION)
+    if (status === 'granted') {
+      alert(`You got permission for using device location`)
+    } else {
+      alert(`You could not get permission for using device location.`)
+    }
+    console.log(permissions)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Check permission" onPress={ () => checkPermission() } />
+      <Button title="Get permission" onPress={ () => getPermission() } />
     </View>
   )
 }
