944a20b025d85fef21b04c04a9a916561150b56c
Yutaka Omido <yutaka.omido@gmail.com>
2019-11-21 08:51:24 +0900

    add local notification

--- App.tsx
+++ App.tsx
@@ -1,32 +1,96 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useEffect } from 'react'
+import { StyleSheet, View, Button } from 'react-native'
+import { Notifications } from 'expo'
 import * as Permissions from 'expo-permissions'
 
 export default function App() {
-  const checkPermission = async function () {
-    const { status, permissions } = await Permissions.getAsync(Permissions.LOCATION)
-    if (status === 'granted') {
-      alert(`You have permission for using device location.`)
-    } else {
-      alert('You do not have permission for using device location.')
+  useEffect(() => {
+    Notifications.addListener(notification => {     
+      console.log(notification)
+      alert(notification.data.title)
+    })
+  }, [])
+
+  const checkPermission = async function (permission: Permissions.PermissionType) {
+    try {
+      const { status, permissions } = await Permissions.getAsync(permission)
+      if (status === 'granted') {
+        alert(`You have permission for using ${permission}.`)
+      } else {
+        alert(`You do not have permission for using ${permission}.`)
+      }
+      console.log(permissions)
+    } catch (err) {
+      console.error(err)
     }
-    console.log(permissions)
   }
 
-  const getPermission = async function () {
-    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION)
-    if (status === 'granted') {
-      alert(`You got permission for using device location`)
-    } else {
-      alert(`You could not get permission for using device location.`)
+  const getPermission = async function (permission: Permissions.PermissionType) {
+    try {
+      const { status, permissions } = await Permissions.askAsync(permission)
+      if (status === 'granted') {
+        alert(`You got permission for using ${permission}`)
+      } else {
+        alert(`You could not get permission for using ${permission}.`)
+      }
+      console.log(permissions)
+    } catch (err) {
+      console.error(err)
     }
-    console.log(permissions)
+  }
+
+  const testSendingNotification = async function () {
+    try {
+      const notificationId = await Notifications.presentLocalNotificationAsync({
+        title: 'テストローカル通知',
+        body: 'ローカル通知の送信テストです',
+        data: {
+          title: 'テストローカル通知',
+          body: 'ローカル通知の送信テストです',
+        },
+        ios: {
+          sound: true,
+          _displayInForeground: true,
+        }
+      })
+      console.log(notificationId)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const testSendingScheduledNotification = async function () {
+    try {
+      const notificationId = await Notifications.scheduleLocalNotificationAsync({
+        title: 'テストローカル通知',
+        body: 'ローカル通知の送信テストです',
+        data: {
+          title: 'テストローカル通知',
+          body: 'ローカル通知の送信テストです',
+        },
+        ios: {
+          sound: true,
+          _displayInForeground: true,
+        }        
+      }, {
+        time: new Date().getTime() + 5000 // 5 seconds later
+      })
+      console.log(notificationId)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Button title="Check permission" onPress={ () => checkPermission() } />
-      <Button title="Get permission" onPress={ () => getPermission() } />
+      <Button title="Check location permission" onPress={ () => checkPermission(Permissions.LOCATION) } />
+      <Button title="Get location permission" onPress={ () => getPermission(Permissions.LOCATION) } />
+      <Button title="Check notification permission" onPress={ () => checkPermission(Permissions.NOTIFICATIONS) } />
+      <Button title="Get notification permission" onPress={ () => getPermission(Permissions.NOTIFICATIONS) } />
+      <Button title="Check user facing notification permission" onPress={ () => checkPermission(Permissions.USER_FACING_NOTIFICATIONS) } />
+      <Button title="Get user facing notification permission" onPress={ () => getPermission(Permissions.USER_FACING_NOTIFICATIONS) } />
+      <Button title="Test sending notification just now" onPress={ () => testSendingNotification() } />
+      <Button title="Test sending notification 5 seconds later" onPress={ () => testSendingScheduledNotification() } />
     </View>
   )
 }
